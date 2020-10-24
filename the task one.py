592ee5ed4ac46883d9c785c345d6670da6c8a7fb
Marat <marat07052002@inbox.ru>
2020-10-24 20:24:10 +0500

    задания к уроку 1

--- the task one.py
+++
@@ -1,8 +0,0 @@
-a = "red"
-b = "dragon"
-print(f"{a}, {b}")
-num1 = int(input(1))
-num2 = int(input(2))
-print(f"you have choosen the number {num1} and {num2}" )
-word = input("Enter any word: ")
-print(f"{word} - its good choise" )
